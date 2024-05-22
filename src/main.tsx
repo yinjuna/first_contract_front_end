51423b78e96d062af707c628ca7d3e90a12f57d9
yinjuna <yinjun@111.com>
2024-05-22 11:26:15 +0800

    update manfiestUrl

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = "https://yinjuna/github.io/first_contract_front_end/tonconnect-manifest.json";
+const manifestUrl = "https://yinjuna.github.io/first_contract_front_end/tonconnect-manifest.json";
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
